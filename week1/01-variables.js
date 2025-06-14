75a100aececf3dfac2dec5a9bdfa013b295d763a
WUHYERI <woohree0125@gmail.com>
2025-06-14 21:07:13 +0900

    [1주차 과제] 변수, 상수 작성
    1. 사용자 이름을 저장할 변수를 선언하고 "자신의 이름"을 할당.
    2. "절대 영도(온도의 최저 한계)인 -273.15°C"를 담을 상수를 선언.
    3. "로그인 여부"를 확인하는 불리언 타입 변수을 선언하고 적절한값을 할당.
    4. "사용자 나이" 변수를 선언하고 숫자 값을 할당.
    5. "상품 가격(예: 39,900원)"을 담는 변수를 작성.
    6. 웹 페이지의 "배경색"을 담는 상수를 작성하고 적절한 값을 할당.
    7. 사용자가 작성한 "댓글 수(예: 12)"를 담는 변수를 선언하고 값을 할당.
    8. "현재 페이지 번호(예: 3)"를 담는 변수를 작성하고 적절한 값을 할당.
    9. "회원 등급('VIP', '골드', '실버' 중 하나)"을 담는 변수를 작성.
    10. "버튼이 클릭되었는지 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정.

---
+++ week1/01-variables.js
@@ -0,0 +1,44 @@
+// ✍ 변수, 상수 작성 과제
+
+// 각 항목에 대해 변수나 상수를 선언하고, 알맞은 값을 할당하세요.
+
+// 1. 사용자 이름을 저장할 변수를 선언하고 "자신의 이름"을 할당하세요.
+let userName = '혜리'
+console.log(userName)
+
+// 2. "절대 영도(온도의 최저 한계)인 -273.15°C"를 담을 상수를 선언하세요.
+const ABSOLUTE_ZERO_CELSIUS = -273.15 + "°C"
+console.log(ABSOLUTE_ZERO_CELSIUS)
+
+// 3. "로그인 여부"를 확인하는 불리언 타입 변수을 선언하고 적절한값을 할당하세요.
+let isLoggedIn = true 
+console.log(isLoggedIn)
+
+// 4. "사용자 나이" 변수를 선언하고 숫자 값을 할당하세요.
+let userAge = 25
+console.log(userAge)
+
+// 5. "상품 가격(예: 39,900원)"을 담는 변수를 작성하세요.
+let price = 39900 + '원'
+console.log(price)
+
+// 6. 웹 페이지의 "배경색"을 담는 상수를 작성하고 적절한 값을 할당하세요.
+const BACKGROUND_COLOR = 'skyblue'
+console.log(BACKGROUND_COLOR) 
+
+// 7. 사용자가 작성한 "댓글 수(예: 12)"를 담는 변수를 선언하고 값을 할당하세요.
+let userCommentCount = 12
+console.log(userCommentCount)
+
+// 8. "현재 페이지 번호(예: 3)"를 담는 변수를 작성하고 적절한 값을 할당하세요.
+let currentPageNumber = 3
+console.log(currentPageNumber)
+
+// 9. "회원 등급('VIP', '골드', '실버' 중 하나)"을 담는 변수를 작성해보세요.
+let memberGrade = 'VIP'
+console.log(memberGrade)
+
+// 10. "버튼이 클릭되었는지 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
+let isButtonClicked = false
+console.log(isButtonClicked)
+console.log(typeof isButtonClicked)
