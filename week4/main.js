f9634039cd5cabec37d12e84eeca2d35f7f3b34d
WUHYERI <woohree0125@gmail.com>
2025-07-06 14:30:25 +0900

    [4주차 과제] : 페이지 새로고침 후 상위 세가지 그림에 바로 적용될 애니메이션 구현 완료

--- week4/main.js
+++ week4/main.js
@@ -1,22 +1,29 @@
+// 모든 이미지
 const images = document.querySelectorAll('img');
 const sun = images[0];
 const cloud01 = images[1];
 const cloud02 = images[2];
 const cat = images[3];
 const ground = images[4];
-
-const banner = document.querySelector('.banner');
-
 console.log(images);
 
+//윗쪽 이미지들
+const topImagesWrapper = document.querySelector('.images__top');
+const topImages = topImagesWrapper.querySelectorAll('img');
+console.log(topImages);
+
+//가장 상위 div요소에 클릭 이벤트리스너 추가
+const banner = document.querySelector('.banner');
+
 banner.addEventListener('click', (evt) => {
-  console.log('click@');
+  console.log('click@@');
 
   if (evt.target === cat) {
     goCat();
   }
 });
 
+//고양이 날아가는 애니메이션
 function goCat() {
   gsap.to(cat, {
     rotation: 360,
@@ -27,5 +34,23 @@ function goCat() {
     yoyo: true,
     duration: 1,
     repeat: 1,
+    ease: 'power1.out',
   });
 }
+
+//상위 세가지 요소들 메인 움직임
+// const mainAnimation = () => {
+//   gsap.fromTo(
+//     topImages,
+
+//     { y: -200, stagger: 0.3 },
+//     { y: 0, stagger: 0.2, ease: 'power1.out' }
+//   );
+// };
+
+const startAnimation = gsap.fromTo(
+  topImages,
+
+  { y: -300, stagger: 0.3 },
+  { y: 0, stagger: 0.2, ease: 'power1.out', duration: 1 }
+);
