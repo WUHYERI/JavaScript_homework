ab41472143a08fdbe1a49960e14717bdf64e4bf2
WUHYERI <woohree0125@gmail.com>
2025-07-13 22:59:10 +0900

    [5주차 과제] : add plan page에 js로 시간, 분 옵션 추가하기
    - css, 마크업 클래스 이름만 조금 수정 완료

--- week5/main.js
+++ week5/main.js
@@ -1,10 +1,23 @@
-const openAddPlanButton = document.querySelector('.add-plan');
-const addPlanPage = document.querySelector('.add-plan-page');
+//main time line
+const mainPage = document.querySelector('.main-page');
+const mainTimeLine = mainPage.querySelector('.time-line');
+const mainTimeLineLists = mainTimeLine.querySelectorAll('li');
+const editToDoButton = mainTimeLine.querySelector('.edit-to-do');
+
+console.log(mainTimeLineLists);
+// add your plan modal
+const addPlanPage = mainPage.querySelector('.add-plan-page');
+
+//버튼들
+const openAddPlanButton = mainPage.querySelector('.add-plan');
 const closeAddPlanPageButton = addPlanPage.querySelector('.close-modal');
+const addToDoListButton = addPlanPage.querySelector('.add-to-do');
+console.log(addToDoListButton);
 
 // add yout plan + 버튼 토글
 openAddPlanButton.addEventListener('click', () => {
   addPlanPage.classList.add('is-open');
+  loadHourMinuteOptions();
 });
 
 closeAddPlanPageButton.addEventListener('click', () => {
@@ -12,4 +25,20 @@ closeAddPlanPageButton.addEventListener('click', () => {
 });
 
 // hour, minute option 추가
-// minute은 30분단위 시간은 01~24
+// minute은 10분단위 시간은 01~24
+const hours = addPlanPage.querySelector('#hourSelect');
+const minutes = addPlanPage.querySelector('#minuteSelect');
+// const hourSection = addPlanPage.querySelector('');
+console.log(minutes);
+const loadHourMinuteOptions = () => {
+  for (let i = 0; i < 25; i++) {
+    const option = document.createElement('option');
+    option.textContent = i.toString().padStart(2, 0);
+    hours.append(option);
+  }
+  for (let i = 0; i < 60; i += 10) {
+    const option = document.createElement('option');
+    option.textContent = i.toString().padStart(2, 0);
+    minutes.append(option);
+  }
+};
